a3ff329a765a4cdc7cd80a059354e9082a0f6489
Scott McCormack <scott.mccormack@articulate.com>
2015-10-29 10:37:15 -0400

    Mucho better collision detection

--- client/components/ball-physics.js
+++ client/components/ball-physics.js
@@ -1,15 +1,12 @@
 import collide from '../util/collide';
 
-function update(game, object) {
-  let handlers = { paddle, wall };
-  collide(game, object, handlers);
+function update(game, ball) {
+  let handlers = { paddle: bouncer, wall: bouncer };
+  collide(game, ball, handlers);
 
-  function paddle(other) {
-    object.vx = -object.vx;
-  }
-
-  function wall(other) {
-    object.vy = -object.vy;
+  function bouncer(side, other) {
+    if (['left', 'right'].indexOf(side) > -1) return ball.vx = -ball.vx;
+    if (['top', 'bottom'].indexOf(side) > -1) return ball.vy = -ball.vy;
   }
 }
 
--- client/util/collide.js
+++ client/util/collide.js
@@ -1,21 +1,39 @@
 import inRange from 'lodash/number/inRange';
+import max from 'lodash/math/max';
 
-function collide(game, object, handlers) {
+function collide(game, A, handlers) {
   game.objects.forEach(checkCollision);
 
-  function checkCollision(other) {
-    if (collided(object, other) && typeof handlers[other.type] === 'function')
-      handlers[other.type](other);
-  }
-}
+  function checkCollision(B) {
+    if (!A.collider || !B.collider || A === B) return;
 
-function collided(A, B) {
-  if (!A.collider || !B.collider || A === B) return false;
-  let overlapX = inRange(A.x, B.x, B.x + B.width) ||
-                 inRange(B.x, A.x, A.x + A.width);
-  let overlapY = inRange(A.y, B.y, B.y + B.height) ||
-                 inRange(B.y, A.y, A.y + A.height);
-  return overlapX && overlapY;
+    let Li = inRange(A.x, B.x, B.x + B.width);
+    let Ri = inRange(B.x, A.x, A.x + A.width);
+    let Ti = inRange(A.y, B.y, B.y + B.height);
+    let Bi = inRange(B.y, A.y, A.y + A.height);
+
+    let Lo = B.x + B.width  - A.x;
+    let Ro = A.x + A.width  - B.x;
+    let To = B.y + B.height - A.y;
+    let Bo = A.y + A.height - B.y;
+
+    let side = (function() {
+      switch (true) {
+        case (Ti && Li):
+          return Lo > To ? 'top' : 'left';
+        case (Ti && Ri):
+          return Ro > To ? 'top' : 'right';
+        case (Bi && Li):
+          return Lo > Bo ? 'bottom' : 'left';
+        case (Bi && Ri):
+          return Ro > Bo ? 'bottom' : 'right';
+        default:
+          return null;
+      }
+    })();
+
+    if (side) handlers[B.type](side, B);
+  }
 }
 
 export default collide;
