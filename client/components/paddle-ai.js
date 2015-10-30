c9f85eb226730be726b44085f6e93dc627a670d7
Scott McCormack <scott.mccormack@articulate.com>
2015-10-30 10:47:27 -0400

    More fun/difficult AI

--- client/components/paddle-ai.js
+++ client/components/paddle-ai.js
@@ -1,10 +1,23 @@
 import find from 'lodash/collection/find';
 
+import collide from '../util/collide';
+
+const difficulty = 0.9;
+let factor, sign;
+randomize();
+
+function randomize() {
+  factor = Math.random() * 0.35;
+  sign   = Math.sign(Math.random() - 0.5);
+}
+
 function update(game, paddle) {
+  collide(game, paddle, { ball: randomize });
   let ball = find(game.objects, { type: 'ball' });
   let by = ball.y + ball.h / 2;
   let py = paddle.y + paddle.h / 2;
-  paddle.vy = Math.sign(by - py) * 0.85 * paddle.vmax;
+  let delta = sign * factor * paddle.h;
+  paddle.vy = Math.sign(by - (py + delta)) * difficulty * paddle.vmax;
 }
 
 export default { update };
