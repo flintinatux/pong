ea7fd04051c7f2120f99b175b1cadfe0df065f34
Scott McCormack <scott.mccormack@articulate.com>
2016-03-31 17:15:17 -0400

    Initial rewrite in Manic

--- client/app.js
+++ client/app.js
@@ -1,18 +1,28 @@
-const Game = require('./classes/game');
+const Manic = require('manic');
 
-let game = window.game = new Game();
+const stage = require('./data/stage.json');
+const types = require('./data/types.json');
 
-document.body.appendChild(game.el);
+var manic = Manic(document.getElementById('game'), 0.6);
 
-let running;
+manic.types(types);
+manic.stage(stage);
 
-window.addEventListener('keydown', function(e) {
-  if (e.keyCode !== 32) return;
-  if (running) {
-    running = false;
-    game.stop();
-  } else {
-    running = true;
-    game.start();
-  }
-});
+// const Game = require('./classes/game');
+
+// let game = window.game = new Game();
+
+// document.body.appendChild(game.el);
+
+// let running;
+
+// window.addEventListener('keydown', function(e) {
+//   if (e.keyCode !== 32) return;
+//   if (running) {
+//     running = false;
+//     game.stop();
+//   } else {
+//     running = true;
+//     game.start();
+//   }
+// });
