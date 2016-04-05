a808cde3b75f59c44c7a7bc0a30f5919c2344314
Scott McCormack <scott.mccormack@articulate.com>
2016-04-05 01:55:34 -0400

    Use scene api, and start paused

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ const keycode = require('keycode');
 const Manic   = require('manic');
 
 const components = require('./data/components.json');
-const stage      = require('./data/stage.json');
+const scenes     = require('./data/scenes');
 const systems    = require('./systems');
 const templates  = require('./data/templates.json');
 
@@ -11,9 +11,9 @@ var manic = Manic(document.getElementById('game'), 0.52);
 manic.components(components);
 manic.templates(templates);
 systems.forEach(manic.system);
-manic.stage(stage);
+manic.scene(scenes.one);
 
-var running = true;
+var running = false;
 
 window.addEventListener('keydown', function(e) {
   if (keycode(e) !== 'space') return;
